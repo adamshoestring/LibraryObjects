bc2d6b62332b7a7ec3f49aa8ab629fdf5e6cd538
WahiMac10 <wahimac10@WahiMac10s-MacBook-Air.local>
2018-11-13 15:52:08 -0500

    first commit

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var library = [
- { author: 'John Grisham', title: 'The Reckoning', readingStatus: true },
- { author: 'Nicholas Sparks', title: 'Every Breath', readingStatus: true },
- { author: 'Stephen King', title: 'Elevation', readingStatus: false }
+ { author: 'Bill Gates', title: 'The Road Ahead', readingStatus: true },
+ { author: 'Steve Jobs', title: 'Walter Isaacson', readingStatus: true },
+ { author: 'Suzanne Collins', title: 'Mockingjay: The Final Book of The Hunger Games', readingStatus: false }
  ];
 
  for(i = 0; i< library.length; i++){
